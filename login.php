272ec08cbddcb675f4f5fa599b42488763f2ee1c
tuscarvalho <tiago.carvalho.sitel@gmail.com>
2017-07-30 23:39:53 -0300

    Tela de Login

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <!--CHAMANDO CSS-->
+        <link rel="stylesheet" href="stylesheets/locastyle.css">
+        <link rel="stylesheet" href="stylesheets/estilo.css">
+        <!--TITULO-->
+        <title>Login - Sistema Promaxi</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    <!--CARREGANDO JS-->
+    <script type="text/javascript" src="javascripts/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="javascripts/locastyle.js"></script>
+    </body>
+</html>
